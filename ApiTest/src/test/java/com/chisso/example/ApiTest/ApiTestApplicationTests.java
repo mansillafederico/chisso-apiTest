ddb937dced58314fb3e4886f6317c0fdd469ff70
Federico Mansilla <mansilla.fede@gmail.com>
2024-06-03 22:44:45 -0300

    First commit

---
+++ ApiTest/src/test/java/com/chisso/example/ApiTest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisso.example.ApiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
